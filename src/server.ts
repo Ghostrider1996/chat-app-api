0476e3e490a1a3b8a6625ed76596f4bb258945eb
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2025-04-05 18:04:56 +0300

    server settup

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
 

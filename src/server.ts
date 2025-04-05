e52e4ffec24a8f8cb5915d41bb494483c8ad9aab
Vasil Petkov <45644418+Ghostrider1996@users.noreply.github.com>
2025-04-05 18:02:31 +0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import cors from "cors";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
